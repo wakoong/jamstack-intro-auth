eb9ec5657ea675da854ccddbb96a0ce7c6f56b23
Woosik Koong <woosik.koong@gmail.com>
2020-06-08 19:05:35 -0700

    Add identity widget

--- src/components/route-login.js
+++ src/components/route-login.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useIdentityContext } from "react-netlify-identity";
+import { navigate } from "gatsby";
 
-const RouteLogin = () => (
-  <>
-    <h1>Login or Signup</h1>
-    <button>Login</button>
-  </>
-);
+const RouteLogin = ({ showModal }) => {
+  const identity = useIdentityContext();
+  if (identity && identity.isLoggedIn) {
+    navigate("/dashboard/secret", { replace: true });
+  }
+  return (
+    <>
+      <h1>Login or Signup</h1>
+      <button onClick={showModal}>Login</button>
+    </>
+  );
+};
 
 export default RouteLogin;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,26 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { navigate } from "gatsby";
 import { Router } from "@reach/router";
+import IdentityModal from "react-netlify-identity-widget";
 import Layout from "../components/layout";
 import Profile from "../components/profile";
 import RouteBase from "../components/route-base";
 import RouteSecret from "../components/route-secret";
 import RouteLogin from "../components/route-login";
 
+import "react-netlify-identity-widget/styles.css";
+
 const Dashboard = ({ location }) => {
+  const [isVisible, setVisibility] = useState(false);
+  
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
       navigate("/dashboard/login", { replace: true });
     }
   }, []);
+
+  const showModal = () => setVisibility(!isVisible);
+
   return (
     <Layout>
       <Profile />
       <Router>
         <RouteBase path="/dashboard/base" />
         <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <RouteLogin path="/dashboard/login" showModal={showModal} />
       </Router>
+      {isVisible && (
+        <IdentityModal
+          role="dialog"
+          showDialog={isVisible}
+          onCloseDialog={showModal}
+        />
+      )}
     </Layout>
   );
 };
